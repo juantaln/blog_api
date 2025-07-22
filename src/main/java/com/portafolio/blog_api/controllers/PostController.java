538dceb996c33d075118ecb60ea718728eb653fe
Juan David Taborda Londoño <jtaborda@sura.com.co>
2025-07-22 14:08:08 -0500

    Coreccion loombok

--- src/main/java/com/portafolio/blog_api/controllers/PostController.java
+++ src/main/java/com/portafolio/blog_api/controllers/PostController.java
@@ -1,73 +1,61 @@
 package com.portafolio.blog_api.controllers;
 
-import com.portafolio.blog_api.entities.Post;
+import com.portafolio.blog_api.dto.PostRequestDto;
+import com.portafolio.blog_api.dto.PostResponseDto;
 import com.portafolio.blog_api.services.PostService;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import java.util.Optional;
 
-@RestController 
-@RequestMapping("/api/posts") 
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
 
-    @Autowired 
+    @Autowired
     private PostService postService;
 
-   
+
     @PostMapping
-    public ResponseEntity<?> createPost(@RequestBody Post post) { 
- 
-        if (post.getUser() == null || post.getUser().getId() == null) {
-            
+    public ResponseEntity<?> createPost(@RequestBody PostRequestDto postRequestDto) {
+        if (postRequestDto.getUserId() == null) {
             return new ResponseEntity<>("El ID del usuario es requerido", HttpStatus.BAD_REQUEST);
         }
-        Long userId = post.getUser().getId();
-        
-      
-        Post newPost = postService.createPost(post, userId);
-        
-        return new ResponseEntity<>(newPost, HttpStatus.CREATED);
+    
+        PostResponseDto createdPost = postService.createPost(postRequestDto); 
+        return new ResponseEntity<>(createdPost, HttpStatus.CREATED);
     }
 
-    
     @GetMapping
-    public ResponseEntity<List<Post>> getAllPosts() {
-        List<Post> posts = postService.getAllPosts();
+    public ResponseEntity<List<PostResponseDto>> getAllPosts() {
+        List<PostResponseDto> posts = postService.getAllPosts();
         return new ResponseEntity<>(posts, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Post> getPostById(@PathVariable Long id) { 
-    Optional<Post> postOptional = postService.getPostById(id);
-
    
-    return postOptional
-            .map(post -> new ResponseEntity<>(post, HttpStatus.OK))
-            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));   
-    }  
-    
-    @PutMapping("/{id}")
-    public ResponseEntity<Post> updatePost(@PathVariable Long id, @RequestBody Post postDetails) {
-    return postService.updatePost(id, postDetails)
-            .map(updatedPost -> new ResponseEntity<>(updatedPost, HttpStatus.OK)) 
-            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));  
-
-
-
+    @GetMapping("/{id}")
+    public ResponseEntity<PostResponseDto> getPostById(@PathVariable Long id) {
+        return postService.getPostById(id)
+                .map(postDto -> new ResponseEntity<>(postDto, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PostResponseDto> updatePost(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto) {
+        return postService.updatePost(id, postRequestDto)
+                .map(updatedPostDto -> new ResponseEntity<>(updatedPostDto, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePost(@PathVariable Long id) {
         if (postService.deletePost(id)) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT); 
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);   
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-
 }
--- src/main/java/com/portafolio/blog_api/dto/PostResponseDto.java
+++ src/main/java/com/portafolio/blog_api/dto/PostResponseDto.java
@@ -13,4 +13,35 @@ public class PostResponseDto {
     private String content;
     private LocalDateTime creationDate;
     private String authorUsername; 
+
+    public Long getId() { 
+        return id;
+     }
+    public void setId(Long id) { 
+        this.id = id; 
+    }
+    public String getTitle() {
+         return title;
+         }
+    public void setTitle(String title) { 
+        this.title = title; 
+    }
+    public String getContent() { 
+        return content;
+     }
+    public void setContent(String content) {
+         this.content = content;
+         }
+    public LocalDateTime getCreationDate() { 
+        return creationDate;
+     }
+    public void setCreationDate(LocalDateTime creationDate) { 
+        this.creationDate = creationDate;
+     }
+    public String getAuthorUsername() { 
+        return authorUsername; 
+    }
+    public void setAuthorUsername(String authorUsername) {
+         this.authorUsername = authorUsername;
+         }
 }
